c3c1e8f8ef1f0e64efeeaab006997b7e2e9a82af
drummondjm <mark.drummond@justice.gov.uk>
2026-04-10 11:29:02 +0100

    fix CVE and update ingress urls

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalcanteenapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalcanteenapi/config/OpenApiConfiguration.kt
@@ -17,9 +17,9 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
   fun customOpenAPI(): OpenAPI = OpenAPI()
     .servers(
       listOf(
-        Server().url("https://digital-canteen-api-dev.hmpps.service.justice.gov.uk").description("Development"),
-        Server().url("https://digital-canteen-api-preprod.hmpps.service.justice.gov.uk").description("Pre-Production"),
-        Server().url("https://digital-canteen-api.hmpps.service.justice.gov.uk").description("Production"),
+        Server().url("https://digital-canteen-api-dev.prison.service.justice.gov.uk").description("Development"),
+        Server().url("https://digital-canteen-api-preprod.prison.service.justice.gov.uk").description("Pre-Production"),
+        Server().url("https://digital-canteen-api.prison.service.justice.gov.uk").description("Production"),
         Server().url("http://localhost:8080").description("Local"),
       ),
     )
