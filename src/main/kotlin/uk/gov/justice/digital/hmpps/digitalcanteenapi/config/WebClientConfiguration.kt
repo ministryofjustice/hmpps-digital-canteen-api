e4f1afdfd2ea3d9a2c93fc2d9363347a7a98a321
drummondjm <mark.drummond@justice.gov.uk>
2026-05-12 12:23:49 +0100

    initial health and medication

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalcanteenapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalcanteenapi/config/WebClientConfiguration.kt
@@ -3,18 +3,30 @@ package uk.gov.justice.digital.hmpps.digitalcanteenapi.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.hmpps.kotlin.auth.authorisedWebClient
 import uk.gov.justice.hmpps.kotlin.auth.healthWebClient
 import java.time.Duration
 
 @Configuration
 class WebClientConfiguration(
-  @param:Value("\${hmpps-auth.url}") val hmppsAuthBaseUri: String,
-  @param:Value("\${api.health-timeout:2s}") val healthTimeout: Duration,
-  @param:Value("\${api.timeout:20s}") val timeout: Duration,
+  @param:Value("\${api.hmpps-auth.base-url}") private val hmppsAuthBaseUri: String,
+  @param:Value("\${api.hmpps-auth.timeout-ms:20s}") private val hmppsAuthHealthTimeout: Duration,
+
+  @param:Value("\${api.health-and-medication.base-url}") val healthAndMedicationBaseUri: String,
+  @param:Value("\${api.health-and-medication.timeout-ms:20s}") val timeout: Duration,
+
+  private val builder: WebClient.Builder,
 ) {
-  // HMPPS Auth health ping is required if your service calls HMPPS Auth to get a token to call other services
   @Bean
   @Suppress("MaxLineLength")
-  fun hmppsAuthHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(hmppsAuthBaseUri, healthTimeout)
+  fun hmppsAuthHealthWebClient(builder: WebClient.Builder): WebClient =
+    builder.healthWebClient(hmppsAuthBaseUri, hmppsAuthHealthTimeout)
+
+  @Bean
+  fun healthAndMedicationWebClient(authorizedClientManager: OAuth2AuthorizedClientManager) =
+    builder.authorisedWebClient(authorizedClientManager, "hmpps-digital-canteen-api",
+      healthAndMedicationBaseUri, timeout)
+
 }
