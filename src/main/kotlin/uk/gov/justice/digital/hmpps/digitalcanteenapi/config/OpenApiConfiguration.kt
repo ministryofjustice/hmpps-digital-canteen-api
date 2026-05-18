cf7c5450ea318e38236fc08f50893bd859b61793
drummondjm <mark.drummond@justice.gov.uk>
2026-05-18 14:25:53 +0100

    updated openapiconfig and test deploy

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalcanteenapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalcanteenapi/config/OpenApiConfiguration.kt
@@ -1,17 +1,26 @@
 package uk.gov.justice.digital.hmpps.digitalcanteenapi.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.OAuthFlow
+import io.swagger.v3.oas.models.security.OAuthFlows
+import io.swagger.v3.oas.models.security.Scopes
+import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenApiConfiguration(buildProperties: BuildProperties) {
-  private val version: String = buildProperties.version!!
+class OpenApiConfiguration(
+  buildProperties: BuildProperties,
+  @param:Value("\${api.hmpps-auth.base-url}") val oauthUrl: String,
+) {
+  private val version: String = buildProperties.version ?: "unknown"
 
   @Bean
   fun customOpenAPI(): OpenAPI = OpenAPI()
@@ -30,12 +39,38 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
       Info().title("HMPPS Digital Canteen Api").version(version)
         .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
     )
-  // TODO Add security schema and roles in `.components()` and `.addSecurityItem()`
-}
+    .components(
+      Components().addSecuritySchemes(
+        "bearer-jwt",
+        SecurityScheme()
+          .type(SecurityScheme.Type.HTTP)
+          .scheme("bearer")
+          .bearerFormat("JWT")
+          .`in`(SecurityScheme.In.HEADER)
+          .name("Authorization"),
+      )
+        .addSecuritySchemes(
+          "hmpps-auth",
+          SecurityScheme()
+            .flows(getFlows())
+            .type(SecurityScheme.Type.OAUTH2)
+            .openIdConnectUrl("$oauthUrl/.well-known/openid-configuration"),
+        ),
+    )
+    .addSecurityItem(SecurityRequirement().addList("bearer-jwt", listOf("read", "write")))
 
-private fun SecurityScheme.addBearerJwtRequirement(role: String): SecurityScheme = type(SecurityScheme.Type.HTTP)
-  .scheme("bearer")
-  .bearerFormat("JWT")
-  .`in`(SecurityScheme.In.HEADER)
-  .name("Authorization")
-  .description("A HMPPS Auth access token with the `$role` role.")
+  fun getFlows(): OAuthFlows {
+    val flows = OAuthFlows()
+    val clientCredflow = OAuthFlow()
+    clientCredflow.tokenUrl = "$oauthUrl/oauth/token"
+    val scopes = Scopes()
+      .addString("read", "Allows read of data")
+      .addString("write", "Allows write of data")
+    clientCredflow.scopes = scopes
+    val authflow = OAuthFlow()
+    authflow.authorizationUrl = "$oauthUrl/oauth/authorize"
+    authflow.tokenUrl = "$oauthUrl/oauth/token"
+    authflow.scopes = scopes
+    return flows.clientCredentials(clientCredflow).authorizationCode(authflow)
+  }
+}
