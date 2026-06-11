0f0331d3ffcfbbe9e838fb44ee734401a99c0e79
drummondjm <mark.drummond@justice.gov.uk>
2026-06-11 14:12:05 +0100

    Adding environments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.3.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.4.0"
   kotlin("plugin.spring") version "2.3.21"
   id("dev.detekt") version "2.0.0-alpha.3"
 }
