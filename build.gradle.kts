4ea5d96833e13cc78e9263a6a54a824a318c004e
sandeep.khajuria <sankhajuria@gmail.com>
2026-07-23 15:18:29 +0100

    CRC-562 -  Upgrade versions to fix high severities and skip detekt for scanning

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.1"
   id("org.openapi.generator") version "7.24.0"
   kotlin("plugin.spring") version "2.4.10"
-  id("dev.detekt") version "2.0.0-alpha.5"
-}
-dependencyCheck {
-  skipConfigurations.add("detekt")
 }
 
 dependencies {
@@ -19,19 +15,6 @@ dependencies {
   testImplementation("org.wiremock:wiremock-standalone:3.13.2")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.43") {
     exclude(group = "io.swagger.core.v3")
-  }
-}
-
-// detekt must use a specific kotlin version when running, this block ensures it's using the correct version
-// this is variation on https://detekt.dev/docs/gettingstarted/gradle/#gradle-runtime-dependencies
-configurations.matching { it.name == "detekt" }.all {
-  resolutionStrategy.eachDependency {
-    if (requested.group == "org.jetbrains.kotlin") {
-      useVersion(
-        dev.detekt.gradle.plugin
-          .getSupportedKotlinVersion(),
-      )
-    }
   }
 }
 
