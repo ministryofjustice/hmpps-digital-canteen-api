3a042c7328fb652337b8370483a9e3e5d372b05c
neeta.shinde <neeta.shinde@justice.gov.uk>
2026-08-10 10:31:42 +0100

    fixed lint errors.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalcanteenapi/mapping/CreateCartRequestMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalcanteenapi/mapping/CreateCartRequestMapper.kt
@@ -4,12 +4,11 @@ import uk.gov.justice.digital.hmpps.digitalcanteenapi.client.medusaclient.dto.Ca
 import uk.gov.justice.digital.hmpps.digitalcanteenapi.client.medusaclient.dto.MedusaCreateCartRequest
 import uk.gov.justice.digital.hmpps.digitalcanteenapi.client.medusaclient.generated.CreateCartRequest
 
-fun CreateCartRequest.toMedusaCreateCartRequest() =
-  MedusaCreateCartRequest(
-    metadata = CartMetadata(
-      prison_id = prisonId,
-      offender_no = offenderNo,
-      first_name = firstName,
-      last_name = lastName,
-    ),
-  )
+fun CreateCartRequest.toMedusaCreateCartRequest() = MedusaCreateCartRequest(
+  metadata = CartMetadata(
+    prison_id = prisonId,
+    offender_no = offenderNo,
+    first_name = firstName,
+    last_name = lastName,
+  ),
+)
