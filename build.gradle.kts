8af1ab5fe891a56abdd7aa96e1cad0fd08e3ef93
sandeep.khajuria <sankhajuria@gmail.com>
2026-09-25 12:35:11 +0100

    CRC-639 - Fixed security issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jlleitschuh.gradle.ktlint.KtlintExtension
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.9"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.10"
   id("org.openapi.generator") version "7.24.0"
   kotlin("plugin.spring") version "2.4.20"
 }
